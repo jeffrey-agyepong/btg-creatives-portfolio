9ff2bfa2c3b61cafe5992a7959771fc789cb08ca
Jeffrey777A <44594550+jeffrey-agyepong@users.noreply.github.com>
2021-03-07 04:53:13 +0000

    .

--- dist/js/init.js
+++ dist/js/init.js
@@ -23,5 +23,14 @@ function showhide()
   var div = document.getElementById("post");
   div.classList.toggle('hidden');
 }
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.carousel');
+  var instances = M.Carousel.init(elems, options);
+});
 
+// Or with jQuery
+
+$(document).ready(function(){
+  $('.carousel').carousel();
+});
 
