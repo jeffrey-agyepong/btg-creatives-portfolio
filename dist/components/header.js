f412c99d34059df4f3102f08570512f3c61b6917
Jeffrey777A <44594550+jeffrey-agyepong@users.noreply.github.com>
2021-03-29 04:32:34 +0000

    .

--- dist/components/header.js
+++ dist/components/header.js
@@ -9,16 +9,24 @@ this.innerHTML = `
 <div class="navbar-fixed">
 <nav class="bg-dark-1 darken-3 nav-center" role="navigation" id="header">
   <div class="nav-wrapper container">
-    <a id="logo-container" href="https://beyondthegravemusic.com" class="brand-logo">Beyond The Grave</a>
+    <a href="https://beyondthegravemusic.com">Beyond The Grave</a>
     <ul class="right hide-on-med-and-down">
-      <li><a href="index.html">Home</a></li>
-      <li><a href="about.html">About</a></li>
-      <li><a href="contact.html">Contact</a></li>
+    <li> <a href="index.html">Introducing</a></li>
+    <li><a href="https://www.beyondthegravemusic.com/tag/Interviews/">Interviews</a></li>
+    <li><a href="https://www.beyondthegravemusic.com/tag/Album-Reviews/">Reviews</a></li>
+    <li><a href="https://www.beyondthegravemusic.com/tag/News/">News</a></li>
+    <li><a href="https://www.beyondthegravemusic.com/submit/">Submit</a></li>
+    <li><a href="https://www.beyondthegravemusic.com/about/">About</a></li>
+    <li><a href="https://www.hire.beyondthegravemusic.com">Hire</a></li>
     </ul>
     <ul id="nav-mobile" class="sidenav black center">
-      <li ><a href="index.html" class="white-text">Home</a></li>
-      <li><a href="about.html" class="white-text">About</a></li>
-      <li><a href="contact.html" class="white-text">Contact</a></li>
+    <li><a href="index.html" class="white-text">Introducing</a></li>
+    <li><a href="https://www.beyondthegravemusic.com/tag/Interviews/" class="white-text">Interviews</a></li>
+    <li><a href="https://www.beyondthegravemusic.com/tag/Album-Reviews/" class="white-text">Reviews</a></li>
+    <li><a href="https://www.beyondthegravemusic.com/tag/News/" class="white-text">News</a></li>    
+    <li><a href="https://www.beyondthegravemusic.com/submit/" class="white-text">Submit</a></li>
+    <li><a href="https://www.beyondthegravemusic.com/about/" class="white-text">About</a></li>
+    <li><a href="https://www.hire.beyondthegravemusic.com" class="white-text">Hire</a></li>
     </ul>
     <a href="#" data-target="nav-mobile" class="sidenav-trigger header_toggle">
       <span></span>
